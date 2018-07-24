3bd52e617fe4a79e684bba5f0a59268388156154
Radu Pascal <haiduc32@gmail.com>
2018-07-24 19:41:46 +0300

    adaugat delete

--- Biblioteca1/Controllers/BooksController.cs
+++ Biblioteca1/Controllers/BooksController.cs
@@ -30,5 +30,47 @@ namespace Biblioteca1.Controllers
 
             return View("AddSuccess");
         }
+
+        //books/delete/15
+        public ActionResult Delete(int id)
+        {
+            var b1 = bookList.First(x => x.Id == id);
+            bookList.Remove(b1);
+
+            return View("List", bookList);
+
+            //bool exitAll = false;
+
+            //for (int i = 0; i < 100; i++)
+            //{
+            //    foreach (BookModel book in bookList)
+            //    {
+            //        if (book.Name == "mybook")
+            //        {
+            //            exitAll = true;
+            //            break;
+            //        }
+
+            //        if (book.Id == id)
+            //        {
+            //            bookList.RemoveAt(id);
+
+            //        }
+            //    }
+
+            //    if (exitAll)
+            //    {
+            //        break;
+            //    }
+            //}
+
+
+        }
+
+        //books/altaction?nume=whitefang&autor=jacklondon
+        //public ActionResult AltAction(string nume, string autor)
+        //{
+
+        //}
     }
 }
